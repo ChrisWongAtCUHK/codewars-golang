7e6b4a0a405556c3776a37332ce9b0a5e5c156e8
Chris Wong <wc38.wong@gmail.com>
2020-06-28 09:13:29 +0800

    Basic tests

--- kata_test.go
+++ kata_test.go
@@ -1,14 +1,23 @@
 package kata
 
 import (
-	"reflect"
 	"testing"
 )
 
-func Test_1(t *testing.T) {
-	result := NextHigher(128)
-	if reflect.DeepEqual(result, 256) == false {
+func equal(t *testing.T, input int, answer int) {
+	if NextHigher(input) != answer {
 		t.Error("Failed")
 	}
+}
+
+func Test_basic(t *testing.T) {
+	equal(t, 129, 130)
+	equal(t, 1, 2)
+	equal(t, 323423, 323439)
+	equal(t, 128, 256)
+	equal(t, 1, 2)
+	equal(t, 1022, 1279)
+	equal(t, 127, 191)
+	equal(t, 1253343, 1253359)
 }
 
