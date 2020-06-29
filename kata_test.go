b95274973938b09fffa3bdb95a23569aefc1f628
Chris Wong <wc38.wong@gmail.com>
2020-06-29 23:31:43 +0800

    Test

--- kata_test.go
+++ kata_test.go
@@ -1,8 +1,17 @@
 package kata
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 )
+
+
+func sol(n int) int {
+    o := n & -n
+    v := n + o
+    return v | ((n ^ v) / o >> 2)
+}
 
 func equal(t *testing.T, input int, answer int) {
 	if NextHigher(input) != answer {
@@ -19,5 +28,14 @@ func Test_basic(t *testing.T) {
 	equal(t, 1022, 1279)
 	equal(t, 127, 191)
 	equal(t, 1253343, 1253359)
+}
+
+func Test_random(t * testing.T) {
+	rand.Seed(time.Now().UnixNano())
+        m := (1 << 30) - 1
+        for i := 0; i < 100; i++ {
+            n := rand.Intn(m) + 1
+			equal(t, n, sol(n))
+		}
 }
 
