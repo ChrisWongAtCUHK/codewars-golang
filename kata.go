5f56552672c0ec8129accdf004fab82543819606
Chris Wong <wc38.wong@gmail.com>
2020-08-15 16:28:43 +0800

    Unlucky Days

--- kata.go
+++ kata.go
@@ -1,16 +1,18 @@
 package kata
 
-// AvramenkoArtem
 import (
-	"strconv"
-	"strings"
+	"time"
 )
 
-func NextHigher(n int) int {
-	for i := n + 1; i > 0; i++ {
-		if strings.Count(strconv.FormatInt(int64(n), 2), "1") == strings.Count(strconv.FormatInt(int64(i), 2), "1") {
-			return i
+func UnluckyDays(year int) int {
+	count := 0;
+	t := time.Date(year, time.January, 13, 0, 0, 0, 0, time.UTC)
+	for i := 0; i < 11; i++ {
+		if int(t.AddDate(0, i, 0).Weekday()) == 5 {
+			count = count + 1
 		}
 	}
-	return 0
+
+	return count;
 }
+
--- kata_test.go
+++ kata_test.go
@@ -1,41 +1,17 @@
 package kata
 
 import (
-	"math/rand"
 	"testing"
-	"time"
 )
 
-
-func sol(n int) int {
-    o := n & -n
-    v := n + o
-    return v | ((n ^ v) / o >> 2)
-}
-
 func equal(t *testing.T, input int, answer int) {
-	if NextHigher(input) != answer {
+	if UnluckyDays(input) != answer {
 		t.Error("Failed")
 	}
 }
 
 func Test_basic(t *testing.T) {
-	equal(t, 129, 130)
-	equal(t, 1, 2)
-	equal(t, 323423, 323439)
-	equal(t, 128, 256)
-	equal(t, 1, 2)
-	equal(t, 1022, 1279)
-	equal(t, 127, 191)
-	equal(t, 1253343, 1253359)
-}
-
-func Test_random(t * testing.T) {
-	rand.Seed(time.Now().UnixNano())
-        m := (1 << 30) - 1
-        for i := 0; i < 100; i++ {
-            n := rand.Intn(m) + 1
-			equal(t, n, sol(n))
-		}
+	equal(t, 2015, 3)
+	equal(t, 1986, 1)
 }
 
